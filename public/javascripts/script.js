49fc80de7dce87f39cf26e5963214678b9ad0608
Gualberto Vannini <gualberto.vannini@gmail.com>
2016-11-24 22:46:58 +0100

    aggiornamento iphone 6 e bottone scoll menù

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -25,3 +25,23 @@ $(document).ready(function () {
 		 });
 
 })
+
+
+
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+                        //se non siamo in cima alla pagina
+            $('#top').fadeIn(); //faccio apparire il box    
+        } else {
+                        //altrimenti (il visitatore è in cima alla pagina scrollTop = 0)
+            $('#top').fadeOut();//Il box scompare
+        }
+    });//Allo scroll function
+ 
+    $('#top').click(function() {
+                //Se clicco sul box torno su (scrollTop:0) con un timing di animazione.
+        $('body,html').animate({scrollTop:0},800);
+    });//Click
+ 
+});//DOM
