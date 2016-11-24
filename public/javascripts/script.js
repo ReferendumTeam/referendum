b8e417057797d0848303e56ba17534139284d670
Matteo Fiorio <matteo.fiorio.vr@gmail.com>
2016-11-24 23:16:29 +0100

    add color to hashtags and mentions

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,27 +1,41 @@
 $(document).ready(function () {
-			$(".main_title button").click(function(){
-				 if ($(this).hasClass('button_y'))
-				 {
-						 $(this).addClass("active");
-						 $('button_n').removeClass("active");
 
-						 $(".yes_box").removeClass("onMobileHide");
-						 $(".no_box").addClass("onMobileHide");
-						 if($(".main_title button.button_n").hasClass('active')){
-							 $(".main_title button.button_n").removeClass("active");
-						 }
-				 }
-			 else if ($(this).hasClass('button_n'))
-				{
-						 $(this).addClass("active");
-						 $('button_y').removeClass("active");
+	$(".main_title button").click(function(){
 
-						 $(".no_box").removeClass("onMobileHide");
-						 $(".yes_box").addClass("onMobileHide");
-						 if($(".main_title button.button_y").hasClass('active')){
-							 $(".main_title button.button_y").removeClass("active");
-						 }
-				 }
-		 });
+		if ($(this).hasClass('button_y'))
+		 
+		 {
+			 $(this).addClass("active");
+			 $('button_n').removeClass("active");
+
+			 $(".yes_box").removeClass("onMobileHide");
+			 $(".no_box").addClass("onMobileHide");
+
+			 if($(".main_title button.button_n").hasClass('active')){
+				 
+				 $(".main_title button.button_n").removeClass("active");
+			 
+			 }
+		 }
+
+	 	else if ($(this).hasClass('button_n'))
+		
+		{
+			 $(this).addClass("active");
+			 $('button_y').removeClass("active");
+
+			 $(".no_box").removeClass("onMobileHide");
+			 $(".yes_box").addClass("onMobileHide");
+
+			 if($(".main_title button.button_y").hasClass('active')){
+				 
+				 $(".main_title button.button_y").removeClass("active");
+			 
+			 }
+		 }
+ 	});
+
+
+ 	
 
 })
